f84fd86d026ee67da802b4b6094e93e57ee71c83
flavynn <flavenhow18@gmail.com>
2023-03-28 01:18:52 +0200

    creating Archetype: Mage, Necromancer, Ranger, Warrior

--- src/Archetypes/index.ts
+++ src/Archetypes/index.ts
@@ -1,3 +1,8 @@
 import Archetype from './Archetype';
+import Necromancer from './Necromancer';
+import Warrior from './Warrior';
+import Mage from './Mage';
+import Ranger from './Ranger';
 
 export default Archetype;
+export { Necromancer, Warrior, Mage, Ranger };
