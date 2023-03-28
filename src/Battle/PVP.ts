8892ae34f4ec8539768800d8476d486eeb340d7b
flavynn <flavenhow18@gmail.com>
2023-03-28 04:04:40 +0200

    Creating PVP.ts

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,22 @@
+import Character from '../Character';
+import Fighter from '../Fighter';
+import Battle from './Battle';
+
+export default class PVP extends Battle {
+  constructor(
+    private player1: Character | Fighter,
+    private player2: Character | Fighter,
+  ) {
+    super(player1);
+  }
+
+  fight(): number {
+    while (this.player1.lifePoints > 0 && this.player2.lifePoints > 0) {
+      this.player1.attack(this.player2);
+      if (this.player2.lifePoints > 0) {
+        this.player2.attack(this.player1);
+      }
+    }
+    return super.fight();
+  }
+}
